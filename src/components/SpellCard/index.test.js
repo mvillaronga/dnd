5492ceadcfc536f68d46ba84d8cc8b912aec8ca0
mvillaronga-medkoder <michael.villaronga@gmail.com>
2019-08-27 00:11:23 -0700

    tests runnign with events

--- src/components/SpellCard/index.test.js
+++ src/components/SpellCard/index.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import SpellCard from '../SpellCard/index';
 import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+
+configure({adapter: new Adapter()});
+
+const mockOnClick = jest.fn()
+
 
 test('Default Spell Card Rendering', () => {
     const component = renderer.create(
-      <SpellCard name="Some Sample Spell" level="1" />,
+      <SpellCard name="Some Sample Spell" level="1" onClick={() => mockOnClick} />,
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  test('It should handle on click', () => {
+  const click = jest.fn()
+  const output = shallow(
+    <SpellCard name="Some Sample Spell" level="1" onClick={click} />,
+  )
+  output.simulate('click')
+  expect(click).toHaveBeenCalled()
+  })
